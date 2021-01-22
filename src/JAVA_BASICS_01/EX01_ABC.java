74438e20e9a685b47a5cb1db3dcdbcf9a4ea1e94
wonho.jeong <jeongwh0310@gmail.com>
2021-01-22 23:53:08 +0900

    2021-01-22 Java Study Commit

--- src/JAVA_BASICS_01/EX01_ABC.java
+++ src/JAVA_BASICS_01/EX01_ABC.java
@@ -1,5 +1,21 @@
 package JAVA_BASICS_01;
-
+//숫자와 문자
 public class EX01_ABC {
-
+	public static void main(String[] args) {
+		//"" 따옴표 없는 숫자는 숫자로 인식한다.
+		System.out.println(1+2);
+		//소수점 2.5
+		System.out.println(1.2+1.3);
+		//곱하기
+		System.out.println(2*5);
+		//나누기
+		System.out.println(6/2);
+		
+		// 문자열
+		System.out.println("안녕");
+		//작은 따옴표 에러남
+//		System.out.println('안녕');
+		System.out.println("안녕 세상");
+//		System.out.println("내가 말했다, "프로그래밍 세계로 잘 왔다고"");
+	}
 }
