8f0d58a1cd360670e0266d0b40e4a36f257aa85b
wonho.jeong <jeongwh0310@gmail.com>
2021-01-23 01:50:01 +0900

    Operator, boolean update

--- src/JAVA_BASICS_01/EX01_ABC.java
+++ src/JAVA_BASICS_01/EX01_ABC.java
@@ -16,6 +16,12 @@ public class EX01_ABC {
 		//작은 따옴표 에러남
 //		System.out.println('안녕');
 		System.out.println("안녕 세상");
+		//문자열에 큰 따옴표를 넣고싶다면?! 따옴표 앞에 \를 넣어주면 된다.
 //		System.out.println("내가 말했다, "프로그래밍 세계로 잘 왔다고"");
+		System.out.println("내가 말했다, \"프로그래밍 세계로 잘 왔다고\"");
+		//줄 띄우기		
+		System.out.println("HTML\nCSS\nJavascript");
+		//문자와 문자를 더할 때는 아래와 같이 한다.
+		System.out.println("생활"+"코딩");
 	}
 }
