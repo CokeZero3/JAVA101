16d160f81b2bb8a889dafb8b317cc93c1ab4f9d6
wonho.jeong <jeongwh0310@gmail.com>
2021-01-28 23:54:47 +0900

    update access & control

--- src/JAVA_BASICS_01/EX13_access_control.java
+++ src/JAVA_BASICS_01/EX13_access_control.java
@@ -1,5 +1,24 @@
 package JAVA_BASICS_01;
 
-public class EX13_access_control {
 
+public class EX13_access_control {
+	public class car{
+		int wheel;
+		String color;
+		String engine;
+		
+		void driving() {
+			
+		}
+	}
+	public class bus extends car{
+		//다중 상속은 안됨 *** 예를 들어 = truck extends car, bus{ 이런식은 에러 발생함.
+		String chair;
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+
+	
 }
