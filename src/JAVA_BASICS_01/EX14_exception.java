acb2457b40a291dbeb7f93079fa29fb67115ad8e
wonho.jeong <jeongwh0310@gmail.com>
2021-01-28 23:59:23 +0900

    20210128 last update

--- src/JAVA_BASICS_01/EX14_exception.java
+++ src/JAVA_BASICS_01/EX14_exception.java
@@ -3,7 +3,7 @@ package JAVA_BASICS_01;
 public class EX14_exception {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/JAVA_BASICS_01/EX15_collection.java
+++ src/JAVA_BASICS_01/EX15_collection.java
@@ -1,5 +1,9 @@
 package JAVA_BASICS_01;
 
 public class EX15_collection {
+	//대망의 collection Framework
+	public static void main(String[] args) {
+		
 
+	}
 }
